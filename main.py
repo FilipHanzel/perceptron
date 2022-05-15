5d51d0238cc9e7ba8fe9b1f265b834ab3b06fff6
Filip <filip.han@interia.pl>
2022-05-15 02:01:51 +0200

    reworked exmaples in for a single model

--- main.py
+++ main.py
@@ -3,27 +3,46 @@ import csv
 import random
 from pprint import pprint
 
-from data_utils import transpose, normalize, encode_as_int, encode
+from data_utils import transpose, normalize, to_categorical, to_binary
 
 from perceptron import Perceptron
-from perceptron import MultilayerPerceptron
 
 
-def load_xor(targets_as_lists: bool = False):
+def load_xor(categorical: bool = False):
     features = [
         [0, 0],
         [0, 1],
         [1, 0],
         [1, 1],
     ]
-    targets = [0, 1, 1, 0]
-    if targets_as_lists:
-        targets = [[value] for value in targets]
 
-    return features, targets
+    if categorical:
+        mapping, targets = to_categorical([0, 1, 1, 0])
+        return features, targets, mapping
+    else:
+        targets = [[0], [1], [1], [0]]
+        return features, targets
 
 
-def load_mpg(targets_as_lists: bool = False):
+def load_sonar(categorical: bool = False):
+    with open(os.path.join("data", "sonar.csv"), "rt") as f:
+        data = [
+            [float(value) for value in features] + [target]
+            for *features, target in [line for line in csv.reader(f) if line]
+        ]
+
+    *features, targets = transpose(data)
+    features = transpose(features)
+
+    if categorical:
+        mapping, targets = to_categorical(targets)
+    else:
+        mapping, targets = to_binary(targets)
+
+    return features, targets, mapping
+
+
+def load_mpg():
     with open(os.path.join("data", "auto-mpg.csv"), "rt") as f:
         data = [
             [float(value) for value in features]
@@ -35,73 +54,161 @@ def load_mpg(targets_as_lists: bool = False):
     data = normalize(data)
 
     targets, *features = transpose(data)
+    targets = [[value] for value in targets]
     features = transpose(features)
-    if targets_as_lists:
-        targets = [[value] for value in targets]
 
     return features, targets
 
 
-def load_sonar(targets_as_lists: bool = False):
-    with open(os.path.join("data", "sonar.csv"), "rt") as f:
-        data = [
-            [float(value) for value in features] + [target]
-            for *features, target in [line for line in csv.reader(f) if line]
-        ]
-
-    *features, targets = transpose(data)
-    features = transpose(features)
-    if targets_as_lists:
-        mapping, targets = encode(targets)
-    else:
-        mapping, targets = encode_as_int(targets)
-
-    return features, targets, mapping
-
-
 if __name__ == "__main__":
-    print("Loading xor data...")
+    print("Running xor (classification)...")
 
-    print("Integer output:")
+    print("Solving with single perceptron as binary classification...")
     features, targets = load_xor()
-    pprint(features)
-    pprint(targets)
+    inputs = 2
+    epochs = 5000
+    base_learning_rate = 0.2
 
-    print("Vector output:")
-    features, targets = load_xor(targets_as_lists=True)
-    pprint(features)
-    pprint(targets)
+    random.seed(0)
 
-    print("Loading sonar data...")
+    model = Perceptron(inputs, [1], "sigmoid", init_method="he")
+    model.train(
+        features,
+        targets,
+        epochs,
+        base_learning_rate,
+        metrics=["binary_accuracy", "sse"],
+    )
 
-    print("Integer output:")
+    for feature, target in zip(features, targets):
+        print(f"{feature=}, {target=}, prediction={model.predict(feature)}")
+
+    print("Solving with single layer as categorical classification...")
+    features, targets, mapping = load_xor(categorical=True)
+    inputs = 2
+    epochs = 5000
+    base_learning_rate = 0.2
+
+    random.seed(0)
+
+    model = Perceptron(inputs, [2], "sigmoid", init_method="he")
+    model.train(
+        features,
+        targets,
+        epochs,
+        base_learning_rate,
+        metrics=["categorical_accuracy", "binary_accuracy", "sse"],
+    )
+
+    for feature, target in zip(features, targets):
+        print(f"{feature=}, {target=}, prediction={model.predict(feature)}")
+
+    print("Solving with MLP...")
+    features, targets, mapping = load_xor(categorical=True)
+
+    inputs = 2
+    epochs = 5000
+    base_learning_rate = 0.2
+
+    random.seed(0)
+
+    model = Perceptron(inputs, [2, 2], "leaky_relu", init_method="he")
+    model.train(
+        features,
+        targets,
+        epochs,
+        base_learning_rate,
+        metrics=["categorical_accuracy", "binary_accuracy", "sse"],
+    )
+
+    for feature, target in zip(features, targets):
+        print(f"{feature=}, {target=}, prediction={model.predict(feature)}")
+
+    print("Running sonar (classification)...")
+
+    print("Solving with single perceptron as binary classification...")
     features, targets, mapping = load_sonar()
-    pprint(mapping)
-    for feature in features[:2]:
-        print(feature)
-    for target in targets[:2]:
-        print(target)
+    inputs = 60
+    epochs = 500
+    base_learning_rate = 0.2
 
-    print("Vector output:")
-    features, targets, mapping = load_sonar(targets_as_lists=True)
-    pprint(mapping)
-    for feature in features[:2]:
-        print(feature)
-    for target in targets[:2]:
-        print(target)
+    random.seed(0)
 
-    print("Loading mpg data...")
+    model = Perceptron(inputs, [1], "sigmoid", init_method="he")
+    model.train(
+        features,
+        targets,
+        epochs,
+        base_learning_rate,
+        metrics=["binary_accuracy", "sse"],
+    )
 
-    print("Integer output:")
+    print("Solving with single layer as categorical classification...")
+    features, targets, mapping = load_sonar(categorical=True)
+    inputs = 60
+    epochs = 500
+    base_learning_rate = 0.2
+
+    random.seed(0)
+
+    model = Perceptron(inputs, [2], "sigmoid", init_method="he")
+    model.train(
+        features,
+        targets,
+        epochs,
+        base_learning_rate,
+        metrics=["categorical_accuracy", "binary_accuracy", "sse"],
+    )
+
+    print("Solving with MLP...")
+    features, targets, mapping = load_sonar(categorical=True)
+    inputs = 60
+    epochs = 500
+    base_learning_rate = 0.2
+
+    random.seed(0)
+
+    model = Perceptron(inputs, [2, 2], "leaky_relu", init_method="he")
+    model.train(
+        features,
+        targets,
+        epochs,
+        base_learning_rate,
+        metrics=["categorical_accuracy", "binary_accuracy", "sse"],
+    )
+
+    print("Running mpg (regression)...")
+
+    print("Solving with single perceptron...")
     features, targets = load_mpg()
-    for feature in features[:2]:
-        print(feature)
-    for target in targets[:2]:
-        print(target)
+    inputs = 7
+    epochs = 500
+    base_learning_rate = 0.01
 
-    print("Vector output:")
-    features, targets = load_mpg(targets_as_lists=True)
-    for feature in features[:2]:
-        print(feature)
-    for target in targets[:2]:
-        print(target)
+    random.seed(0)
+
+    model = Perceptron(inputs, [1], "sigmoid", init_method="he")
+    model.train(
+        features,
+        targets,
+        epochs,
+        base_learning_rate,
+        metrics=["binary_accuracy", "sse"],
+    )
+
+    for feature, target in zip(features[:5], targets[:5]):
+        print(f"{target=}, prediction={model.predict(feature)}")
+
+    print("Solving with MLP...")
+    features, targets = load_mpg()
+    inputs = 7
+    epochs = 500
+    base_learning_rate = 0.1
+
+    random.seed(0)
+
+    model = Perceptron(inputs, [10, 5, 1], "leaky_relu", init_method="he")
+    model.train(features, targets, epochs, base_learning_rate)
+
+    for feature, target in zip(features[:5], targets[:5]):
+        print(f"{target=}, prediction={model.predict(feature)}")
