91d17088520ed111f6d7002ac6d6c57635accf06
Filip <filip.han@interia.pl>
2022-05-26 17:58:07 +0200

    removed unnecessary functions from data_utils

--- perceptron/data_utils.py
+++ perceptron/data_utils.py
@@ -16,47 +16,6 @@ def shuffle(features: List, targets: List) -> Tuple[List, List]:
     return features, targets
 
 
-def normalize(data: List[List[float]]) -> List[List[float]]:
-    columns = transpose(data)
-    total_min = []
-    total_max = []
-    for column in columns:
-        total_min.append(min(column))
-        total_max.append(max(column))
-
-    normalized_dataset = [
-        [
-            (column - min_) / (max_ - min_)
-            for column, min_, max_ in zip(record, total_min, total_max)
-        ]
-        for record in data
-    ]
-
-    return normalized_dataset
-
-
-def drop_columns(data, column_index: Union[int, List[int]]):
-    """Drop columns of the data (inplace). Supports negative indexes."""
-    if isinstance(column_index, int):
-        column_index = [column_index]
-
-    record_length = len(data[0])
-
-    # Support negative indexes
-    for index in column_index:
-        if not (-record_length <= index < record_length):
-            raise IndexError("index out of range")
-    column_index = [
-        index if index >= 0 else record_length + index for index in column_index
-    ]
-
-    for index in sorted(column_index, reverse=True):
-        for line in data:
-            del line[index]
-
-    return data
-
-
 def to_binary(column: List[str]) -> List[List[int]]:
     """Encode column values as 0 or 1."""
     values = set(column)
